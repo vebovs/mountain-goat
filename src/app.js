6a7ebf292cdd419dfad3e2f9899ca2c7a38193d1
vebovs <vebovs@hotmail.no>
2020-06-07 22:03:27 +0200

    deployment

--- src/app.js
+++ src/app.js
@@ -160,6 +160,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('./build'));
     app.get('*', (req, res) => {
         console.log(__dirname);
+        console.log(path.resolve(__dirname, './build', 'index.html'));
         res.sendFile(path.resolve(__dirname, './build', 'index.html'));
     });
 } else {
