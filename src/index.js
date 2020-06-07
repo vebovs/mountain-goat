17404b311937358544416a64c1ce942511232aaf
vebovs <vebovs@hotmail.no>
2020-06-07 19:36:21 +0200

    minor naming scheme change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import { Main } from './components/Main/main';
+import { Main } from './Components/Main/main';
 
 const root = document.getElementById('root');
 if(root != null){
