87cb1cd6769e6ce657290929792dc6bfa47c5cc4
vebovs <vebovs@hotmail.no>
2020-06-07 21:04:26 +0200

    deployment

--- src/app.js
+++ src/app.js
@@ -162,4 +162,6 @@ if(process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(5000, () => console.log('Server started at port 5000'));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server started at port ' + port));
