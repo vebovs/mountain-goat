961d381c703f30bad2dceef92cd738f454e3a86b
vebovs <vebovs@hotmail.no>
2021-08-14 18:37:38 +0200

    added slider

--- client/src/pages/Map.tsx
+++ client/src/pages/Map.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Page from '../components/Page';
 import type { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import {
@@ -9,6 +8,34 @@ import {
   useMapEvent,
   ZoomControl,
 } from 'react-leaflet';
+import {
+  Box,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from '@chakra-ui/react';
+import Page from '../components/Page';
+
+const InputSlider = () => {
+  return (
+    <Box
+      position='absolute'
+      zIndex='overlay'
+      bottom='0'
+      top='0'
+      right='0'
+      marginRight='6'
+    >
+      <Slider aria-label='slider-ex-3' defaultValue={20} orientation='vertical'>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+    </Box>
+  );
+};
 
 const LocationCirle = () => {
   const [point, setPoint] = useState<LatLngExpression>([59.858264, 5.783487]);
@@ -23,20 +50,23 @@ const Map = () => {
 
   return (
     <Page>
-      <MapContainer
-        style={{ height: '100%' }}
-        center={[59.858264, 5.783487]}
-        zoom={zoom}
-        zoomControl={false}
-        doubleClickZoom={false}
-      >
-        <TileLayer
-          attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <ZoomControl position='bottomright' />
-        <LocationCirle />
-      </MapContainer>
+      <Box h='full'>
+        <MapContainer
+          style={{ height: '100%', position: 'relative' }}
+          center={[59.858264, 5.783487]}
+          zoom={zoom}
+          zoomControl={false}
+          doubleClickZoom={false}
+        >
+          <TileLayer
+            attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <ZoomControl position='topleft' />
+          <LocationCirle />
+        </MapContainer>
+        <InputSlider />
+      </Box>
     </Page>
   );
 };
