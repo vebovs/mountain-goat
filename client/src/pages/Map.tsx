50fdc779d51f439209ddffece9448e59f1bd6d13
vebovs <vebovs@hotmail.no>
2021-08-14 13:43:13 +0200

    added the circle

--- client/src/pages/Map.tsx
+++ client/src/pages/Map.tsx
@@ -1,15 +1,31 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Page from '../components/Page';
+import type { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
+import {
+  Circle,
+  MapContainer,
+  TileLayer,
+  useMapEvent,
+  ZoomControl,
+} from 'react-leaflet';
+
+const LocationCirle = () => {
+  const [point, setPoint] = useState<LatLngExpression>([59.858264, 5.783487]);
+  const map = useMapEvent('click', (event) => {
+    setPoint([event.latlng.lat, event.latlng.lng]);
+  });
+  return <Circle center={point} radius={250} />;
+};
 
 const Map = () => {
   const zoom: number = 14;
+
   return (
     <Page>
       <MapContainer
         style={{ height: '100%' }}
-        center={[63.418265, 10.402862]}
+        center={[59.858264, 5.783487]}
         zoom={zoom}
         zoomControl={false}
         doubleClickZoom={false}
@@ -19,6 +35,7 @@ const Map = () => {
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         <ZoomControl position='bottomright' />
+        <LocationCirle />
       </MapContainer>
     </Page>
   );
