a6952c47042efbd541eb0cf45b9c96babb8a5c84
vebovs <vebovs@hotmail.no>
2021-08-14 12:04:37 +0200

    added the map

--- client/src/components/Page.tsx
+++ client/src/components/Page.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { Center } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 type PageProps = {
   children: JSX.Element;
 };
 
-const Page = ({ children }: PageProps) => <Center h='full'>{children}</Center>;
+const Page = ({ children }: PageProps) => (
+  <Box bg='pink.200' h='full'>
+    {children}
+  </Box>
+);
 
 export default Page;
--- client/src/pages/Map.tsx
+++ client/src/pages/Map.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import { Text } from '@chakra-ui/react';
 import Page from '../components/Page';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 
-const Map = () => (
-  <Page>
-    <Text>Map</Text>
-  </Page>
-);
+const Map = () => {
+  const zoom: number = 14;
+  return (
+    <Page>
+      <MapContainer
+        style={{ height: '100%' }}
+        center={[63.418265, 10.402862]}
+        zoom={zoom}
+        zoomControl={false}
+        doubleClickZoom={false}
+      >
+        <TileLayer
+          attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <ZoomControl position='bottomright' />
+      </MapContainer>
+    </Page>
+  );
+};
 
 export default Map;
