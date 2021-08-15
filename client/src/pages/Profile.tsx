8dcbad9aa44859ff1a0b693d8205e2fdec574bdf
vebovs <vebovs@hotmail.no>
2021-08-15 13:41:58 +0200

    added tab shifting between sign in and sign up

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
 import Page from '../components/Page';
 import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
 
 const Profile = () => (
   <Page>
     <Box bg='white' color='black'>
-      <SignIn />
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>Sign In</Tab>
+          <Tab>Sign Up</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <SignIn />
+          </TabPanel>
+          <TabPanel>
+            <SignUp />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   </Page>
 );
