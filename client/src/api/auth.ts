92bd5500644ae2671ceabcf440f9385d12ec5e99
vebovs <vebovs@hotmail.no>
2021-08-15 21:32:30 +0200

    added auth api

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,1 +1,17 @@
 import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000'
+});
+
+export const registerUser = (username: string, password: string) => api.post('/register', {
+    username: username,
+    password: password
+}).then(res => res.data);
+
+export const loginUser = (username: string, password: string) => api.post('/login', {
+    username: username,
+    password: password
+}).then(res => res.data);
+
+export const logoutUser = () => api.get('/logout').then(res => res.data);
