fbf103b3040a985b40beec8011770caed3e9121f
vebovs <vebovs@hotmail.no>
2021-08-15 13:52:53 +0200

    added signup markup

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,7 +1,34 @@
 import * as React from 'react';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Box,
+  Button,
+} from '@chakra-ui/react';
 
 const SignUp = () => {
-  return null;
+  return (
+    <Box mr='12' ml='12'>
+      <FormControl>
+        <FormLabel>Username</FormLabel>
+        <Input />
+        <FormHelperText></FormHelperText>
+        <FormErrorMessage></FormErrorMessage>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <Input />
+        <FormHelperText></FormHelperText>
+        <FormErrorMessage></FormErrorMessage>
+      </FormControl>
+      <Button mt='2' w='100%' colorScheme='blue'>
+        Sign up
+      </Button>
+    </Box>
+  );
 };
 
 export default SignUp;
