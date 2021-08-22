b2060ac07abc9116c6ff82c95b586a685f132c33
vebovs <vebovs@hotmail.no>
2021-08-22 12:11:15 +0200

    centered signin and signup tabs

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,27 +1,37 @@
 import * as React from 'react';
-import { Box, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from '@chakra-ui/react';
 import Page from '../components/Page';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 
 const Profile = () => (
   <Page>
-    <Box bg='white' color='black' h='full'>
-      <Tabs isFitted variant='enclosed' h='full'>
-        <TabList mb='1em'>
-          <Tab>Sign In</Tab>
-          <Tab>Sign Up</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <SignIn />
-          </TabPanel>
-          <TabPanel>
-            <SignUp />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Box>
+    <Center bg='green.400' color='black' h='full'>
+      <Box bg='white' borderWidth='1px' borderRadius='lg' w='600px' h='400px'>
+        <Tabs isFitted variant='enclosed'>
+          <TabList mb='1em'>
+            <Tab>Sign In</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <SignIn />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Center>
   </Page>
 );
 
