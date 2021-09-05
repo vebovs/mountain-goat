121eaf37368ea313d4fb0d67ffd28d0c8f3aaa20
vebovs <vebovs@hotmail.no>
2021-09-05 19:43:54 +0200

    added custom feature type and error handling if user does not input hike nickname

--- client/src/components/MapDropdown.tsx
+++ client/src/components/MapDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   IconButton,
@@ -17,6 +17,15 @@ import {
   CloseIcon,
 } from '@chakra-ui/icons';
 import type { Polyline } from 'leaflet';
+import type { ObjectId } from 'mongodb';
+import { Geometry } from 'geojson';
+
+type CustomFeatureType = {
+  _id: ObjectId;
+  geometry: Geometry;
+  type: string;
+  properties: object;
+};
 
 type MapDropdownProps = {
   Path: Polyline[];
@@ -26,6 +35,19 @@ type MapDropdownProps = {
 
 const MapDropdown = ({ SetPathing, Path, SetPath }: MapDropdownProps) => {
   const [showDropdown, setShowDropdown] = useState<boolean>(true);
+  const [nickname, setNickname] = useState<string>('');
+  const [errorStatus, setErrorStatus] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  useEffect(() => {
+    if (errorStatus) {
+      const interval = setInterval(() => {
+        setErrorStatus(false);
+        setErrorMessage('');
+      }, 2500);
+      return () => clearInterval(interval);
+    }
+  }, [errorStatus, setErrorStatus, errorMessage, setErrorMessage]);
 
   return (
     <Box
@@ -44,11 +66,17 @@ const MapDropdown = ({ SetPathing, Path, SetPath }: MapDropdownProps) => {
       />
       <Collapse in={showDropdown}>
         <Box bg='white' borderWidth='1px' borderRadius='lg' p='2' mt='2'>
-          <FormControl>
+          <FormControl isInvalid={errorStatus}>
             <FormLabel>Hike nickname</FormLabel>
-            <Input type='text' />
-            <FormHelperText></FormHelperText>
-            <FormErrorMessage></FormErrorMessage>
+            <Input
+              type='text'
+              value={nickname}
+              onChange={(event) => setNickname(event.target.value)}
+            />
+            <FormHelperText mb='2'>
+              Set a unique nickname for the hike.
+            </FormHelperText>
+            <FormErrorMessage mb='2'>{errorMessage}</FormErrorMessage>
           </FormControl>
           <Flex>
             <IconButton
@@ -56,6 +84,17 @@ const MapDropdown = ({ SetPathing, Path, SetPath }: MapDropdownProps) => {
               colorScheme='blue'
               icon={<StarIcon />}
               mr='2'
+              onClick={() => {
+                if (!nickname) {
+                  setErrorStatus(true);
+                  setErrorMessage('Nickname required.');
+                } else {
+                  const Ids = Path.map(
+                    (p) => (p.feature as CustomFeatureType)._id,
+                  );
+                  console.log(Ids);
+                }
+              }}
             />
             <IconButton
               aria-label='exit favorite hike creation dropdown'
