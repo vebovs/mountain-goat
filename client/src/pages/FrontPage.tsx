a315a688619af7dba02a503363a27cd84238b4e3
vebovs <vebovs@hotmail.no>
2021-09-08 20:01:42 +0200

    front page layout and styling

--- client/src/pages/FrontPage.tsx
+++ client/src/pages/FrontPage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Center, Text } from '@chakra-ui/react';
+import { Center, Text, Link, Flex } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import Page from '../components/Page';
 
 const FrontPage = () => {
   return (
     <Page>
-      <Center bg='white' color='black' h='full'>
-        <Text>Mountain Goat</Text>
+      <Center bg='green.50' color='black' h='full'>
+        <Flex direction='column'>
+          <Text fontSize='6xl' as='abbr' color='green.700'>
+            Mountain Goat
+          </Text>
+          <Link href='https://github.com/vebovs/mountain-goat' isExternal>
+            GitHub <ExternalLinkIcon mx='2px' />
+          </Link>
+        </Flex>
       </Center>
     </Page>
   );
