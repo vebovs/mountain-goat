534fca352a86bb4c3daa1f2f2c84eb49b25ece38
vebovs <vebovs@hotmail.no>
2021-09-11 14:01:55 +0200

    changed cors settings

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ store.on('error', () => {
     console.log(error);
 });
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
